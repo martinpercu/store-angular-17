b4b37d490da16e68723151c9272dd64f6d727fc9
Martin Mendez <martinpercu@gmail.com>
2023-12-11 16:26:05 -0500

    connect the list to product and viceversa

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  // imageSrc = 'https://picsum.photos/500/500?r=' + Math.random();
+  @Input({required: true}) imageSrc: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    console.log('clicking from the child');
+    this.addToCart.emit('este produto goes to the cart ==> ' + this.title + "\n" + "this is the price ===> " + this.price);
+  }
 
 }
