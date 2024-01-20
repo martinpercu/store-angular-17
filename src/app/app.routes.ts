2ce7ffe7c28a536b77f643e838dfa406c613eb60
Martin Mendez <martinpercu@gmail.com>
2024-01-20 19:13:52 -0300

    implementation of product detail page with first connexion from main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 
-import { ListComponent } from './domains/products/pages/list/list.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
+import { ListComponent } from '@products/pages/list/list.component';
+import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 import { LayoutComponent } from '@shared/components/layout/layout.component'
+
+import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
+
 
 export const routes: Routes = [
   {
@@ -18,6 +21,10 @@ export const routes: Routes = [
       {
         path: 'about',
         component: AboutComponent
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent
       }
     ]
   },
